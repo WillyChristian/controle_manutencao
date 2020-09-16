328df1217c877588faedd17b677c81454f609560
WillyChristian <crisaort@gmail.com>
2020-09-15 23:56:31 -0300

    Add an api to retrieve data from google sheets

--- pages/api/app.js
+++ pages/api/app.js
@@ -1,14 +1,28 @@
 require('dotenv').config
 const { GoogleSpreadsheet } = require('google-spreadsheet')
-const credential = require('../../credentials.json')
-const doc = new GoogleSpreadsheet("1ZSQAkbstQaIpJvEWvKuemakN-Bne-yGkJwZLCBVC5wk")
+const doc = new GoogleSpreadsheet(process.env.SHEET_ID)
 
 export default async (req, res) => {
     try{
-        await doc.useServiceAccountAuth(credential)
+        const email = process.env.GOOGLE_ACCOUNT_EMAIL
+        const key = process.env.GOOGLE_PRIVATE_KEY
+        await doc.useServiceAccountAuth({
+            client_email: email,
+            private_key: key
+        })
         await doc.loadInfo()
-        await 
-        res.send(doc.title)
+        const sheet = await doc.sheetsByIndex[0]
+        await sheet.loadCells('A1:O6')
+
+        // geting all line info
+        const agenda = []
+        for(let i = 1; i <= 14; i++ ){
+            const status = sheet.getCell(3,i).value
+            const time = sheet.getCell(2,i).value
+            agenda.push({
+                time,status
+            })
+        }
     }catch(err){
         console.log(err)
     }
